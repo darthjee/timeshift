f0fb12709b4855c81a5b66cdfe1fc8875f6fe02e
Fernando Favini <darthjee@gmail.com>
2015-05-09 17:53:09 -0700

    Working router controller

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,5 +2,9 @@
   var app = angular.module('router', []);
 
   app.controller('RoutesController', function() {
+    this.setPage = function(page) {
+      this.page = page + '?ajax=true';
+    };
+    this.setPage('/');
   });
 })();
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,9 @@
 class StaticController < ApplicationController
   include HighVoltage::StaticPage
+
+  layout :layout_for_page
+
+  def layout_for_page
+    params[:ajax] ? false : 'application'
+  end
 end
