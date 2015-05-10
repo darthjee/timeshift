93e1b847dbe1e43fa4944a5cd383088f0cb63ebd
Fernando Favini <darthjee@gmail.com>
2015-05-09 19:11:31 -0700

    Add login controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resource :login, only: [:create, :destroy] do
+    get '/' => :new
+  end
+
   scope controller: 'static', action: 'show' do
     get '/', id: 'home', as: :home
   end
