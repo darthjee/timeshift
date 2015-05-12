993e58ee580dd9d7afa9bdd0c71e0760879bf5b6
darthjee <darthjee@gmail.com>
2015-05-12 18:25:57 -0300

    Refactor login.js

--- app/assets/javascripts/modules/login.js
+++ app/assets/javascripts/modules/login.js
@@ -1,15 +1,29 @@
 (function() {
+  function Login($scope, $http) {
+    this.scope = $scope;
+    this.http = $http;
+    _.bindAll(this, '_redirect');
+  }
+
+  var fn = Login.prototype;
+
+  fn.login = function() {
+    this.requestLogin().success(this._redirect);
+  };
+
+  fn.requestLogin = function() {
+    return this.http.post('/login.json', {
+      user: {
+        email: this.email
+      }
+    });
+  };
+
+  fn._redirect = function(data){
+    this.scope.$emit('redirect:start', data.redirect);
+  };
+
   var app = angular.module('login', ['requester']);
 
-  app.controller('LoginController', ['$rootScope','$requester', function($scope, $http) {
-    this.login = function() {
-      $http.post('/login.json', {
-        user: {
-          email: this.email
-        }
-      }).success(function(data){
-      	$scope.$emit('redirect:start', data.redirect);
-      });
-    };
-  }]);
+  app.controller('LoginController', ['$rootScope','$requester', Login]);
 })();
