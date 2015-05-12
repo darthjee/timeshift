51acc8bb8b6c893487c8c804cd3b4937e502fcd4
darthjee <darthjee@gmail.com>
2015-05-12 18:14:00 -0300

    Refactor router controller

--- app/assets/javascripts/modules/router.js
+++ app/assets/javascripts/modules/router.js
@@ -1,15 +1,25 @@
 (function() {
+  function Router($scope) {
+    var controller = this;
+    _.bindAll(this, 'setPage');
+    this.scope = $scope;
+    this._bind();
+  }
+
+  var fn = Router.prototype;
+
+  fn.setPage = function(e, page) {
+    if (typeof page == 'undefined') {
+      page = e;
+    }
+    this.page = page + '?ajax=true';
+  };
+
+  fn._bind = function() {
+    this.scope.$on('redirect:start', this.setPage);
+  };
+
   var app = angular.module('router', []);
 
-  app.controller('RoutesController', ['$rootScope', function($scope) {
-    var controller = this;
-
-    $scope.$on('redirect:start', function(e, path) {
-      controller.setPage(path);
-    });
-
-    controller.setPage = function(page) {
-      controller.page = page + '?ajax=true';
-    };
-  }]);
+  app.controller('RoutesController', ['$rootScope', Router]);
 })();
