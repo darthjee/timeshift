43b7b1f5e6c7cff2614d7629e8bd429e66876bd5
darthjee <darthjee@gmail.com>
2023-05-14 12:37:01 -0300

    Rename source folder
    Add both source files
    
    Remove bundle install for now
    
    Add new project

--- source/app/controllers/application_controller.rb
+++ source/app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
-  include Azeroth::Resourceable
-
-  helper Magicka::Helper
-
-  def forbidden
-    head :forbidden
-  end
 end
