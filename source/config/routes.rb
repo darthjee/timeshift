7ba3c8978d3ae56fae2f84e9341a06dbb9903192
darthjee <darthjee@gmail.com>
2023-05-26 10:07:50 -0300

    Add routes

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/' => 'home#show', as: :home
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index] do
+    collection do
+      resources :login, only: [:create] do
+        get '/' => :check, on: :collection
+      end
+      delete '/logoff' => 'login#logoff'
+    end
+  end
 end
