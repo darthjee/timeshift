7205177182a48d15198af4c583773f26617c6e50
Fernando Favini <darthjee@gmail.com>
2023-05-26 13:11:22 +0200

    Magicka (#10)

---
+++ source/config/initializers/magicka.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Magicka::Form.with_element('Magicka::NgSelect', :ng_select)
+Magicka::Display.with_element('Magicka::NgSelectText', :ng_select)
+Magicka::Display.with_element('Magicka::NgPagination', :pagination)
