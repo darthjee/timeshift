ba02273568fba7d229f69ce3b3bb1345d74c8e56
darthjee <darthjee@gmail.com>
2023-07-31 07:45:25 -0300

    Add chain settable

--- source/app/models/settings.rb
+++ source/app/models/settings.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Settings
-  extend Sinclair::EnvSettable
+  extend Sinclair::ChainSettable
 
-  settings_prefix 'TIMESHIFT'
+  source :env, EnvSettings
 
   with_settings(
     :password_salt,
