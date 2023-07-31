000e5c8ec89fe8cb55a2e75d53777c3a6b1bb905
darthjee <darthjee@gmail.com>
2023-07-31 07:52:37 -0300

    Add seconds caster

--- source/app/models/settings.rb
+++ source/app/models/settings.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+class Sinclair::Settable::Caster
+  cast_with(:seconds, &:seconds)
+end
+
 class Settings
   extend Sinclair::ChainSettable
 
@@ -7,8 +11,8 @@ class Settings
 
   with_settings(:password_salt)
   setting_with_options(:hex_code_size, default: 16, type: :integer)
-  setting_with_options(:session_period, default:  2.days, type: :integer)
-  setting_with_options(:cache_age, default: 10.seconds, type: :integer)
+  setting_with_options(:session_period, default:  2.days, type: :seconds)
+  setting_with_options(:cache_age, default: 10.seconds, type: :seconds)
   setting_with_options(:title, default: 'Timeshift')
   setting_with_options(:favicon, default: '/favicon.ico')
 end
