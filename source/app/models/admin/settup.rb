98c50f9235282ee4c5e1702963dacb602dc189f9
darthjee <darthjee@gmail.com>
2023-09-26 14:14:08 -0300

    Return all settings

--- source/app/models/admin/settup.rb
+++ source/app/models/admin/settup.rb
@@ -4,22 +4,26 @@ module Admin
   class Settup < Sinclair::Model
     initialize_with :admin, :settings
 
-    SETTINGS=%i[].freeze
+    SETTINGS=%i[set favicon title cache_age session_period].freeze
 
     class << self
       def attribute_names
-        [:admin]
+        [:admin, :settings]
       end
 
       def all
         self.new(
           admin: User.admin.find_or_initialize_by({}),
-          settings: ActiveSetting.all 
+          settings: all_settings
         )
       end
 
-      def all_settings
+      private
 
+      def all_settings
+        SETTINGS.map do |key|
+          [key, ActiveSetting.find_by(key: key)&.value]
+        end.to_h
       end
     end
   end
